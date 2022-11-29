11e3b526cd7c6befae9dccd27ea24a8b09721a2d
Rasya Ravi <Rasya Ravi@ARAA.attlocal.net>
2022-11-29 12:20:51 -0600

    test

---
+++ Test/src/test/java/Logout.java
@@ -0,0 +1,25 @@
+
+
+
+
+import io.cucumber.java.en.*;
+
+
+
+public class Logout  {
+
+	@Given("Admin\\/User\\/Staff Logged on to LMS website {string}")
+	public void admin_user_staff_logged_on_to_lms_website(String string) {
+	
+	}
+		
+	@When("Admin\\/User\\/Staff  Clicks on LogOut button")
+	public void admin_user_staff_clicks_on_log_out_button() {
+	 
+	}
+
+	@Then("Admin\\/User\\/Staff should be navigated to Login Screen")
+	public void admin_user_staff_should_be_navigated_to_login_screen() {
+	 
+	}
+}
